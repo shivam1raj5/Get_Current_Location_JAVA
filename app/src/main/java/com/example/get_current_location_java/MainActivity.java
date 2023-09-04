8a547ac18bdad69ae48ab5b237be61f6dcfbbcf8
Shivam Raj <shivam1raj5@gmail.com>
2023-09-04 20:09:45 +0530

    first commit

---
+++ app/src/main/java/com/example/get_current_location_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.get_current_location_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Get_Current_Location_JAVA"
+include(":app")
