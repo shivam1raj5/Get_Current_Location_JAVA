88eb75950654ee278cc529671565eddb7f6bcb39
Shivam Raj <shivam1raj5@gmail.com>
2023-09-07 22:54:04 +0530

    Movement Distance calculation is added

--- app/src/main/java/com/example/get_current_location_java/MainActivity.java
+++ app/src/main/java/com/example/get_current_location_java/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.get_current_location_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -7,6 +8,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -30,6 +32,17 @@ import com.karumi.dexter.listener.single.PermissionListener;
 public class MainActivity extends AppCompatActivity {
 
     SupportMapFragment smf;
+
+    LatLng latLngM;
+
+    double latitudeM;
+
+    double longitudeM;
+
+   // double latitudeN;
+
+   // double longitudeN;
+    LatLng latLngN;
     FusedLocationProviderClient client;
 
     @Override
@@ -46,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        getmylocation();
+                        getmylocation1();
                     }
 
                     @Override
@@ -63,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void getmylocation() {
+    public void getmylocation1() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -81,12 +94,15 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(final Location location) {
                 smf.getMapAsync(new OnMapReadyCallback() {
                     @Override
-                    public void onMapReady(GoogleMap googleMap) {
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
                         if (location != null) {
-                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here...!!");
+                            latLngM = new LatLng(location.getLatitude(), location.getLongitude());
+                            latitudeM = location.getLatitude();
+                            longitudeM = location.getLongitude();
+                            MarkerOptions markerOptions = new MarkerOptions().position(latLngM).title("Your Initial Point!!");
                             googleMap.addMarker(markerOptions);
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngM, 20));
+                            refresh();
                         } else {
                             Toast.makeText(MainActivity.this, "Please on your phone location app permission", Toast.LENGTH_SHORT).show();
                         }
@@ -95,4 +111,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void getmylocation2() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                smf.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
+                        if (location != null) {
+                            latLngN = new LatLng(location.getLatitude(), location.getLongitude());
+                            double latitudeN = location.getLatitude();
+                            double longitudeN = location.getLongitude();
+
+                            double earthRadius = 6371000; //meters
+                            double dLat = Math.toRadians(latitudeM-latitudeN);
+                            double dLng = Math.toRadians(longitudeN-longitudeM);
+                            double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                                    Math.cos(Math.toRadians(latitudeM)) * Math.cos(Math.toRadians(latitudeN)) *
+                                            Math.sin(dLng/2) * Math.sin(dLng/2);
+                            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                            float dist = (float) (earthRadius * c);
+
+                                Toast.makeText(MainActivity.this, "Distance Traveled : "+dist, Toast.LENGTH_SHORT).show();
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngN, 20));
+                        } else {
+                            Toast.makeText(MainActivity.this, "Please on your phone location app permission", Toast.LENGTH_SHORT).show();
+                        }
+                        refresh();
+                    }
+                });
+            }
+        });
+    }
+    private void refresh() {
+         Handler handler = new Handler();
+         Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                getmylocation2();
+            }
+        };
+        handler.postDelayed(runnable , 3000);
+    }
+
 }
